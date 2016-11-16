8931a4c5d30ca6016690c4861cced37e06996dd2
Jacob Mortensen <jacob@jacobmortensen.com>
2016-11-15 17:21:49 -0900

    add movements

--- Nightmare1024.js
+++ Nightmare1024.js
@@ -6,7 +6,6 @@ const GameState = require('./GameState');
 Nightmare.action('updateBoard', function nightmareUpdateBoard(gameState, done) {
   this.evaluate(() => {
     const tiles = Array.from(document.querySelectorAll('.tile-container')[0].children);
-    console.log(tiles.map(tile => tile.className));
     return tiles.map(tile => tile.className);
   }, gameState, done)
   .then((tileClasses) => {
@@ -22,7 +21,34 @@ Nightmare.action('updateScore', function nightmareUpdateScore(gameState, done) {
   .then((scoreHtml) => {
     gameState.setScore(GameState.parseGameScoreString(scoreHtml));
     console.log(`score:${gameState.totalScore}`);
+    done();
   });
+});
+
+Nightmare.action('moveLeft', function nightmareMoveLeft(done) {
+  this.move('a')
+  .then(() => done());
+});
+
+Nightmare.action('moveRight', function nightmareMoveRight(done) {
+  this.move('d')
+  .then(() => done());
+});
+
+Nightmare.action('moveUp', function nightmareMoveRight(done) {
+  this.move('w')
+  .then(() => done());
+});
+
+Nightmare.action('moveDown', function nightmareMoveRight(done) {
+  this.move('s')
+  .then(() => done());
+});
+
+Nightmare.action('move', function nightmareMove(key, done) {
+  this.type('.tile-container', key)
+  .then(() => this.wait('.tile-new'))
+  .then(() => done());
 });
 
 module.exports = Nightmare;
--- index.js
+++ index.js
@@ -21,32 +21,17 @@ class Driver {
       .wait('.tile')
       .updateBoard(this.gameState)
       .updateScore(this.gameState)
+      .moveDown()
+      .updateBoard(this.gameState)
+      .updateScore(this.gameState)
       .then((args) => {
         console.log(`done args:${args}`);
       })
-      // .evaluate((getBoard) => {
-      //   // const tiles = Array.from(document.querySelectorAll('.tile-container')[0].children);
-      //   // return tiles.map(tile => tile.className);
-      //   return getBoard(document);
-      // }, this.getBoard)
-      // .then(tileClasses => this.updateBoard(tileClasses))
       // .then(() => this.nightmare.type('.tile-container', 'a'))
-      // .then(() => this.nightmare.evaluate(this.getBoard))
       .catch((error) => {
         console.error(`error: ${error}`);
       });
   }
-
-  // getBoard(document) {
-  //   const tiles = Array.from(document.querySelectorAll('.tile-container')[0].children);
-  //   return tiles.map(tile => tile.className);
-  // }
-
-  // updateBoard(tileClasses) {
-  //   this.gameState.setBoard(GameState.parseBoard(tileClasses));
-  //   this.gameState.logBoard();
-  //   return Promise.resolve();
-  // }
 }
 
 const driver = new Driver();
